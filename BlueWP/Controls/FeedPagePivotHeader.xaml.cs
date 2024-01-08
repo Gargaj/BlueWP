647a0b47cb85cce09ff65418f219fe8bed6e13f9
Gargaj <gargaj@conspiracy.hu>
2024-01-08 23:55:42 +0100

    change pivot headers to icons

---
+++ BlueWP/Controls/FeedPagePivotHeader.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BlueWP.Controls
+{
+  public sealed partial class FeedPagePivotHeader : UserControl
+  {
+    public FeedPagePivotHeader()
+    {
+      InitializeComponent();
+      DataContext = this;
+    }
+
+    public static readonly DependencyProperty GlyphProperty = DependencyProperty.Register("Glyph", typeof(string), typeof(FeedPagePivotHeader), null);
+
+    public string Glyph
+    {
+      get { return GetValue(GlyphProperty) as string; }
+      set { SetValue(GlyphProperty, value); }
+    }
+
+    public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(FeedPagePivotHeader), null);
+
+    public string Label
+    {
+      get { return GetValue(LabelProperty) as string; }
+      set { SetValue(LabelProperty, value); }
+    }
+  }
+}

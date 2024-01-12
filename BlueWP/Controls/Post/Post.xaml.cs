620478ce7a461d1507c47c727eeb95d666515963
Gargaj <gargaj@conspiracy.hu>
2024-01-12 15:18:18 +0100

    prepare stuff for profile viewing

--- BlueWP/Controls/Post/Post.xaml.cs
+++ BlueWP/Controls/Post/Post.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace BlueWP.Controls.Post
+﻿using Windows.UI.Xaml.Controls;
+
+namespace BlueWP.Controls.Post
 {
   public partial class Post : PostBase
   {
@@ -6,6 +8,11 @@
     {
       InitializeComponent();
       LayoutRoot.DataContext = this;
+    }
+
+    private void ViewProfile_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    {
+      // TODO
     }
   }
 }

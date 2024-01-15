1bc234e95b054fcdaf09c75e2623f8df3507ae4e
Gargaj <gargaj@conspiracy.hu>
2024-01-15 14:59:08 +0100

    add search lexicons

---
+++ BlueWP.ATProto/Lexicons/App/BSky/Actor/SearchActors.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace BlueWP.ATProto.Lexicons.App.BSky.Actor
+{
+  /// <see cref="https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/actor/searchActors.json"/>
+  public class SearchActors : ILexicon
+  {
+    public string EndpointID => "app.bsky.actor.searchActors";
+
+    //public string term; // DEPRECATED
+    public string q;
+    public uint? limit;
+    public string cursor;
+  }
+  public class SearchActorsResponse : ILexicon
+  {
+    public string EndpointID => "app.bsky.actor.searchActors";
+
+    public string cursor;
+    public List<Defs.ProfileView> actors;
+  }
+}
